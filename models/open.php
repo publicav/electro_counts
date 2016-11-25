0fd8737f402c3baeb3f6abd13150071d40880a78
publicav <publicav@gmail.com>
2016-11-26 00:10:22 +0200

    electro_testing 1.01

--- models/open.php
+++ models/open.php
@@ -5,14 +5,26 @@
 //  mysql_select_db("electro_count",$db);
 //  mysql_query("SET NAMES 'utf8'");
 
-$db_li = new mysqli("localhost", "root", "1234", "electro_count");
-if ($db_li->connect_errno) {
-    echo "Не удалось подключиться к MySQL: " . $mysqli->connect_error;
-}
-if (!$db_li->set_charset("utf8")) {
-    printf("Ошибка при загрузке набора символов utf8: %s\n", $mysqli->error);
-    exit();
-} 
+// $db_li = new mysqli("localhost", "root", "1234", "electro_count");
+// if ($db_li->connect_errno) {
+//     echo "Не удалось подключиться к MySQL: " . $mysqli->connect_error;
+// }
+// if (!$db_li->set_charset("utf8")) {
+//     printf("Ошибка при загрузке набора символов utf8: %s\n", $mysqli->error);
+//     exit();
+// } 
+
+$host = "localhost";
+$db = "electro_count";
+$charset = "utf8";
+$user = "root";
+$pass = "1234";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+$pdo = new PDO($dsn, $user, $pass, $opt);
 
 
  $keys1 = 'CoUnTeRs';
