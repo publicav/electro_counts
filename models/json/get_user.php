18a74109242aca65642736adae95417ad17f99e0
publicav <publicav@gmail.com>
2016-11-26 00:50:27 +0200

    electro_count pdo get_user.php

--- models/json/get_user.php
+++ models/json/get_user.php
@@ -2,14 +2,22 @@
 include_once("../open.php");
 include_once("../config.php");
 include_once("../funclib.php");
-	
+
+$user_all = array();
+
 $sq = "SELECT id, users, name, family FROM users WHERE  (ring > 0) ORDER BY id DESC;";
-if ($res = $db_li->query($sq)) {
-    while ($row = $res->fetch_assoc()) {
-        $user_all[] = $row;              
-    }
-    $res->free();
+$res = $pdo->query( $sq );
+
+while ($row = $res->fetch()) {
+    $user_all[] = $row;
 }
+
+// if ($res = $db_li->query($sq)) {
+//     while ($row = $res->fetch_assoc()) {
+//         $user_all[] = $row;              
+//     }
+//     $res->free();
+// }
 
 $type['success'] = true;
 $type['id_error'] = 0;
