17f3d05153a87f9fd127b816c7ebb0e17d430774
publicav <publicav@gmail.com>
2016-11-26 12:58:40 +0200

    input type=time to type=text

--- add_value.php
+++ add_value.php
@@ -24,7 +24,7 @@ include_once("models/json/lots.php");
 		<div class="p_input"><div class="label_p"><label for="substation">Подстанция</label></div><select id="substation" class="input_selected"></select></div>
 		<div class="p_input"><div class="label_p"><label for="counter">Ячейка</label></div><select id="counter" class="input_selected"></select></div>
         
-		<div class="p_input"><div class="label_p"><label for="path_name"> Время замера</label></div><input type="text"  class="input_date_b" id="date_airing_begin"  name="date_begin" value="<?php echo  date("d-m-Y");;?>"/><input   class="input_time_b" id="time_airing_begin" type="time" name="time_begin"/></div>
+		<div class="p_input"><div class="label_p"><label for="path_name"> Время замера</label></div><input type="text"  class="input_date_b" id="date_airing_begin"  name="date_begin" value="<?php echo  date("d-m-Y");;?>"/><input   class="input_time_b" id="time_airing_begin" type="text" name="time_begin"/></div>
         <div class="p_input"><div class="label_p"><label for="counter_val">Значение счётчика</label></div><input id="counter_val"  type="" class="input_form" name="counter_val"/></div>        
 		<button id="ok_f"  type="submit" class="button">Ok</button>
 		<button id="edit_f"  type="submit" class="button" disabled="disabled">Правка</button>
--- views/base/edit_form.php
+++ views/base/edit_form.php
@@ -5,7 +5,7 @@
 		<div class="p_input"><div class="label_p"><label for="substation_edit">Подстанция</label></div><select id="substation_edit" class="input_selected"></select></div>
 		<div class="p_input"><div class="label_p"><label for="counter_edit">Ячейка</label></div><select id="counter_edit" class="input_selected"></select></div>
         
-		<div class="p_input"><div class="label_p"><label for="path_name"> Время замера</label></div><input type="text"  class="input_date_b" id="date_airing_begin_edit"  name="date_begin" value="<?php echo  date("d-m-Y");;?>"/><input   class="input_time_b" id="time_airing_begin_edit" type="time" name="time_begin"/></div>
+		<div class="p_input"><div class="label_p"><label for="path_name"> Время замера</label></div><input type="text"  class="input_date_b" id="date_airing_begin_edit"  name="date_begin" value="<?php echo  date("d-m-Y");;?>"/><input   class="input_time_b" id="time_airing_begin_edit" type="text" name="time_begin"/></div>
         <div class="p_input"><div class="label_p"><label for="counter_val">Значение счётчика</label></div><input id="counter_val_edit"  type="" class="input_form" name="counter_val"/></div>        
 		<input id="ok_f"  type="submit"  tabindex="-1" style="position:absolute; top:-1000px">
 	</form>
