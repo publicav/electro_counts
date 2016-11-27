89c77812952ad00346ece326408d1c62518fdcf1
publicav <publicav@gmail.com>
2016-11-27 02:35:51 +0200

    done get_counter_filtred.php

--- models/json/get_counter_filtred.php
+++ models/json/get_counter_filtred.php
@@ -13,20 +13,20 @@ foreach ($_GET as $key => $value) {
 if (isset($get_prog['data'])) $substation = (int)$get_prog['data']; else
 {
     header("HTTP/1.1 400 Bad Request", true, 400);    
-    echo exit_error(false, 1, 'Error input Lot ');
+    echo exit_error(false, 1, 'Error input substation ');
     exit();
 }
 
 $sq  = "SELECT c.id, c.name FROM  count AS c WHERE (c.substations = ?);";
 $counts_count[] = Array('id' => '0','name' => 'Все ячейки');
 $res = $pdo->prepare( $sq );
- if ($res->execute( [$lot] )) {
-    while ($row = $res->fetch()) $counts_substation[] = $row;
+ if ($res->execute( [$substation] )) {
+    while ($row = $res->fetch()) $counts_count[] = $row;
 } else {
     header("HTTP/1.1 400 Bad Request", true, 400);
     print exit_error( false, 3, $res->errorInfo() );
     exit();
 }
-$result = array('success'=> true, 'error' => 'Ok', 'id_error' => 0,  'data'=>$counts_substation);
+$result = array('success'=> true, 'error' => 'Ok', 'id_error' => 0,  'data'=>$counts_count);
 print json_encode($result);
 ?>
