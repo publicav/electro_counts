9546b7833bec3f027fd69fd9bca433a49268d6a9
valikpk\valik <publicav@gmail.com>
2017-01-06 11:53:37 +0200

    Начало редактирования user_privelege.php

--- models/json/user_privelege.php
+++ models/json/user_privelege.php
@@ -19,34 +19,33 @@ $dataCheck = explode(",", $get_prog['data']);
 
 $sq = "SELECT m.id_a AS id_a, m.id_menu AS id_menu, m.name AS name, m.url AS url FROM menu_left AS m;"; 
 
-    if ($res = $db_li->query($sq)) {
-        while ($row = $res->fetch_assoc()) {
-            $menu_left_m[] = $row;
-        }
-        $res->free();
-    }  else {
-		$type['success'] = false;
-		$type['id_error'] = 3;
-		$type['error'] = 'mysql_error()';
-		echo json_encode($type);
-		exit();
-	}
+$res = $pdo->prepare( $sq );
+if ($res->execute()) {
+	$menu_left_m = $res->fetchAll(); 
+} else {
+	header("HTTP/1.1 400 Bad Request", true, 400);
+	print exit_error( false, 3, $res->errorInfo()[2] );
+	exit();
+}
+// print_r($menu_left_m);
+for($i = 0; $i < SizeOf($menu_left_m); $i++) {
+    $privelege[] = array('id_users' => $get_prog['id_user']  , 'id_menu' => $menu_left_m[$i]['id_menu'], 'visibly' => $dataCheck[$i]);
+}
+// print_r($privelege);
 
-for($i = 0; $i < SizeOf($menu_left_m); $i++)
-{
-    $privelege[] = Array('id_users' => $get_prog['id_user']  , 'id_menu' => $menu_left_m[$i]['id_menu'], 'visibly' => $dataCheck[$i]);
+$sq = "SELECT t_priv.id FROM tables_priv AS t_priv WHERE (t_priv.id_users = :id_users);"; 
+$param = array ('id_users' => $get_prog['id_user'] ); 
+$res = $pdo->prepare( $sq );
+if ($res->execute( $param )) {
+	$id_privelege = $res->fetchAll(); 
+} else {
+	header("HTTP/1.1 400 Bad Request", true, 400);
+	print exit_error( false, 3, $res->errorInfo()[2] );
+	exit();
 }
 
-$sq = "SELECT t_priv.id FROM tables_priv AS t_priv WHERE (t_priv.id_users = '" . $get_prog['id_user'] .  "');"; 
-
-    if ($res = $db_li->query($sq)) {
-        while ($row = $res->fetch_assoc()) {
-            $id_privelege[] = $row;
-        }
-        $res->free();
-    }
-
-if (isset($id_privelege)) {
+ 
+if (!empty($id_privelege)) {
     for($i = 0; $i < SizeOf($privelege); $i++) 
     {
         $privelege_var = 'visibly = ' . $privelege[$i]['visibly'];
@@ -61,8 +60,7 @@ if (isset($id_privelege)) {
         
     }	
 } else {
-    for($i = 0; $i < SizeOf($privelege); $i++) 
-    {
+    for($i = 0; $i < SizeOf($privelege); $i++) {
             $count = 0;  
             if ($i == 0)  $vars = "("; else $vars = ",(";        
             foreach ($privelege[$i] as $key => $value) 
@@ -83,7 +81,7 @@ if (isset($id_privelege)) {
 		exit();
 	}	
     
-    
+  
 }
 
 $type['id_error'] = 0;
