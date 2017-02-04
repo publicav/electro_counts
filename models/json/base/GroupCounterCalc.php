37830b0a982bfd0a130c74a4c3fbb8e12cad76c1
publicav <publicav@gmail.com>
2017-02-04 15:33:36 +0200

    Работа с классом GroupCounterCalc и модифицирование класса DivisionDay.

--- models/json/base/GroupCounterCalc.php
+++ models/json/base/GroupCounterCalc.php
@@ -9,6 +9,7 @@
 namespace base;
 
 use pdo\GroupCounterData;
+use date\DivisionDay;
 
 class GroupCounterCalc {
     private $_dataGroup;
@@ -16,6 +17,7 @@ class GroupCounterCalc {
     private $_nameGroup;
     private $_inputSqlData;
     private $_sortData;
+    private $_cnIndex;
     private $_calcData;
 
     public function __construct( $numberGroup, $dateLow, $dateHigh ) {
@@ -61,45 +63,70 @@ class GroupCounterCalc {
         $dtCurrent = new \DateTime( $this->_dateLow );
         while ( $dtCurrent <= $dtHigh ) {
             $timeStamp = $dtCurrent->getTimestamp();
-            $this->_bisectionCounter( $timeStamp );
-            //            var_dump( $timeStamp );
+            $this->_bisectionCounters( $timeStamp );
+            $this->_calc();
+            //            var_dump( $cnIndex );
             $dtCurrent->add( new \DateInterval( 'P1D' ) );
         }
     }
 
-    private function _bisectionCounter( $timeStamp ) {
+    private function _bisectionCounters( $timeStamp ) {
         foreach ( $this->_sortData as $key => $sortDataCount ) {
-            //            var_dump( $key );
-            $this->_bisection( $timeStamp, $sortDataCount );
-            $test = [ 1, 2, 3, 4, 5 ];
-            var_dump( (int)( count( $test ) / 2 ) );
+            $retIndex[ $key ] = $this->_bisection( $timeStamp, $sortDataCount );
+            //            var_dump( $retIndex, date( 'Y-m-d h:i:s', $timeStamp ),
+            //                $sortDataCount[ $retIndex['index1'] ], $sortDataCount[ $retIndex['index2'] ] );
         }
+        $this->_cnIndex = $retIndex;
     }
 
     private function _bisection( $timeStamp, $counter ) {
-        $index = count( $counter );
-        //        var_dump($counter[ $index - 1 ]['date_second']  );
+        $index = count( $counter ) - 1;
+        //        var_dump( $counter[ $index ] );
         if ( $index <= 2 ) {
-            $retIndex = [ 'error' => 1, 'index1' => null, 'index2' => null ];
+            $retIndex = [ 'error' => 1, 'index1' => null, 'index' => null, 'index2' => null ];
             return $retIndex;
         }
-        if ( $timeStamp >= $counter[ $index - 1 ]['date_second'] ) {
-            $retIndex = [ 'error' => 0, 'index1' => $index - 2, 'index2' => $index - 1 ];
+        if ( $timeStamp >= $counter[ $index ]['date_second'] ) {
+            $retIndex = [ 'error' => 0, 'index1' => $index - 1, 'index' => 0, 'index2' => $index ];
             return $retIndex;
         }
         if ( $timeStamp <= $counter[0]['date_second'] ) {
-            $retIndex = [ 'error' => 0, 'index1' => 0, 'index2' => 1 ];
+            $retIndex = [ 'error' => 0, 'index1' => 0, 'index' => 0, 'index2' => 1 ];
             return $retIndex;
         }
-        $index = ( ( $index - 1 ) / 2 );
-        var_dump( $index );
-        //        while ( 1 ) {
-        //            if ( ( $timeStamp <= $counter[ $index ] ) and ( $timeStamp >= $counter[ $index - 1 ] ) ) {
-        //                break;
-        //            }
-        //
-        //        }
-
+        $index = (int)( $index / 2 );
+        $position = $index;
+        $count = 0;
+        while ( 1 ) {
+            $timeInd = $counter[ $position ]['date_second'];
+            $timeInd1 = $counter[ $position - 1 ]['date_second'];
+            //            var_dump( $position, $timeStamp / 100 / 1000, $timeInd / 100 / 1000 );
+            if ( ( $timeStamp <= $timeInd ) and ( $timeStamp >= $timeInd1 ) ) {
+                break;
+            }
+            $index = (int)( $index / 2 );
+            if ( $timeStamp <= $timeInd ) {
+                $position = $position - $index;
+            } else {
+                $position = $position + $index;
+            }
+            if ( $count >= 11 ) break;
+            $count++;
+        }
+        return [ 'error' => 0, 'indexLow' => $position - 1, 'index' => $position, 'indexHigh' => $position + 1 ];
     }
 
+    protected function _calc() {
+        //        $indexLow = $
+        foreach ( $this->_sortData as $key => $sortDataCount ) {
+            $indexArr = $this->_cnIndex[ $key ];
+            $indexLow = $indexArr['indexLow'];
+            $indexHigh = $indexArr['indexHigh'];
+            $index = $indexArr['index'];
+
+            $DivisionDay = new DivisionDay( $sortDataCount[ $index ]['dt1'] );
+
+            var_dump( $DivisionDay );
+        }
+    }
 }
--- models/json/date/DivisionDay.php
+++ models/json/date/DivisionDay.php
@@ -1,22 +1,35 @@
 <?php
 namespace date;
-class DivisionDay { 
-    public $minuteBefore = 0 ;
+
+class DivisionDay {
+    public $minuteBefore = 0;
     public $minuteAfter = 0;
-    private $dt1, $dt2;
-    private $interval;
 
     /**
      * DivisionDay constructor.
      * @param $dt1
      */
-    function __construct($dt1 ) {
-        $this->dt2 = date("Y-m-d", strtotime( $dt1 )) . ' 00:00:00';
-		$this->datetime1 = new \DateTime( $dt1 );
-        $this->datetime2 = new \DateTime( $this->dt2 );
-        $this->interval = $this->datetime1->diff( $this->datetime2 );
+    function __construct( $dt1 ) {
+        $dt_tek = new \DateTime( $dt1 );
+        $datetime1 = new \DateTime( $dt1 );
+        $datetime2 = new \DateTime( $dt_tek->format( 'Y-m-d' ) );
+        $interval = $datetime1->diff( $datetime2 );
 
-        $this->minuteBefore = $this->interval->h * 60 +  $this->interval->i;
-        $this->minuteAfter =  1440 -  $this->minuteBefore;
-    }   
+        $this->minuteBefore = $interval->h * 60 + $interval->i;
+        $this->minuteAfter = 1440 - $this->minuteBefore;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAfter() {
+        return $this->minuteAfter;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBefore() {
+        return $this->minuteBefore;
+    }
 }
