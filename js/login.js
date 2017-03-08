de8030173a5b30b2c6bb4161fe4e89feeb9d4229
ValikPk\valik <publicav@gmail.com>
2017-03-08 20:46:30 +0200

    Переписан раздел регистрации login.js Part1

--- js/login.js
+++ js/login.js
@@ -1,15 +1,97 @@
 $( function () {
-
     menuLeftInit();
+    const ReqestData = {
+        render: {},
+        data: '',
+        url: '',
+        type: '',
+        init: function ( render, url, param = '', type = 'post' ) {
+            this.data = param;
+            this.render = render;
+            this.url = url;
+            this.type = type;
+        },
+        reqest: function () {
+            let me = this;
+            //noinspection JSUnresolvedVariable
+            $.ajax( { dataType: 'json', type: me.type, url: me.url, data: me.data } )
+                .done( ( result ) => {
+                    console.log( result );
+                    this.render.doRun( result.data );
+                    this.render.render();
+                } )
+                .fail( ( result ) => alert( result.responseJSON.error ) );
 
-    $( '#loginform' ).submit( function ( e ) {
-        e.preventDefault();
-        console.log( this );
-        const me = this;
-        registration( me );
-        login_form.dialog( "close" );
-    } );
+        },
+    };
+    const MainMenu = {
+        dest: {},
+        html: '',
+        init: function ( dest ) {
+            this.dest = dest;
+        },
+        doRun: function ( data ) {
+            let st = '';
+            st += '<ul>';
+            for ( let i = 0; i < data.length; i ++ ) { //noinspection JSUnresolvedVariable
+                st += `	<li class="menu_childs1">
+                                <a id="${data[ i ].id_a}" href="${data[ i ].id_a}">${data[ i ].name}</a>
+                            </li>
+            `;
+            }
+            st += '</ul>';
+            this.html = st;
+        },
+        render: function () {
+            this.dest.html( this.html );
+        }
+    };
+    const LeftMenu = {
+        dest: {},
+        html: '',
+        init: function ( dest ) {
+            this.dest = dest;
+        },
+        doRun: function ( data ) {
+            let st = '';
+            let li_id = '';
+            st += '<nav id="navbar"><ul id="left-menu">';
+            for ( let i = 0; i < data.length; i ++ ) {
+                let lm = data[ i ][ 0 ];
+                //noinspection JSUnresolvedVariable
+                if ( lm.li_id !== null ) li_id = " class=" + lm.li_id; else li_id = '';
+                //noinspection JSUnresolvedVariable
+                st += `	<li${li_id}>
+    			<a id="${lm.id_a}" href="${lm.id_a}">
+    				<i class="${lm.icon}"></i>
+    				${lm.name}
+    			</a>
+    		</li>`;
+                if ( data[ i ].length > 1 ) {
+                    let j = 1;
+                    st += `<ul class="submenu hide-submenu">`;
+                    while ( j < data[ i ].length ) {
+                        var subLm = data[ i ][ j ];
+                        st +=
+                            `<li>
+                            <a  id="${subLm.id_a}" href="${subLm.id_a}">
+                                <i class="${subLm.icon}"></i>
+                                ${subLm.name}
+                            </a>
+                        </li>`;
+                        j ++;
+                    }
+                    st += "</ul>";
+                }
+            }
 
+            st += '</ul></nav>';
+            this.html = st;
+        },
+        render: function () {
+            this.dest.html( this.html );
+        }
+    };
     const login_form = $( '#loginmodal' ).dialog( {
         title: "Регистрация пользователя",
         autoOpen: false,
@@ -18,104 +100,56 @@ $( function () {
         height: 250,
         width: 400
     } );
-
-    $( '#menu' ).on( 'click', '#login', function ( e ) {
-        login_form.dialog( "open" );
-        e.preventDefault();
-    } );
-
-    $( '#menu' ).on( 'click', '#logout', function ( e ) {
-        unregistration();
-        e.preventDefault();
-    } );
-
     const unregistration = () => {
         $.ajax( { dataType: 'json', type: 'post', url: 'ajax/unregistration/' } )
-            .done( ( result ) => {
-                $.ajax( { dataType: 'json', type: 'post', url: 'json/menu.json' } )
-                    .done( ( result_menu ) => {
-                        let menu = result_menu.menu;
-                        $( '#menu' ).html( `<ul>${print_mainmenu( menu )}</ul>` );
-                        $( '#left' ).html( '' );
-                        $( '#right' ).html( '' );
-                    } )
-                    .fail( () => alert( 'Error' ) );
-                alert( result.message );
+            .done( () => {
+                MainMenu.init( $( '#menu' ) );
+                ReqestData.init( MainMenu, 'json/menu.json' );
+                ReqestData.reqest();
+
+                $( '#left' ).html( '' );
+                $( '#right' ).html( '' );
             } )
             .fail( () => alert( 'Error' ) );
     }
-
     const registration = ( form ) => {
         var m_method = $( form ).attr( 'method' );
         var m_action = $( form ).attr( 'action' );
         var m_data = $( form ).serialize();
 
+        //noinspection JSUnresolvedVariable
         $.ajax( { dataType: 'json', type: m_method, url: m_action, data: m_data } )
             .done( ( result ) => {
-                $.ajax( { dataType: 'json', type: 'post', url: 'json/menu_registration.json' } )
-                    .done( ( result_menu ) => {
-                        let menu = result_menu.menu;
-                        let mainfile = `<ul>${print_mainmenu( menu )}`;
-                        if ( result.id != 0 ) mainfile += `<div class="user"><div class="title_user">Вы зашли как:</div>${result.name} ${result.family}</div>`;
-                        mainfile += '</ul>';
-                        $( '#menu' ).html( mainfile );
-                    } )
-                    .fail( ( result ) => alert( 'Error' ) );
+                let userName = '';
+                MainMenu.init( $( '#menu' ) );
+                ReqestData.init( MainMenu, 'json/menu_registration.json' );
+                ReqestData.reqest();
 
-                $.ajax( { dataType: 'json', type: 'post', url: 'ajax/menuleft/' } )
-                    .done( ( result_menu ) => {
-                        var menu = result_menu;
-                        $( '#left' ).html( `<nav id="navbar"><ul id="left-menu">${print_menu( menu )}</ul></nav>` );
-                    } )
-                    .fail( ( result ) => {
-                        console.log( result );
-                        //alert(result.responseJSON.error);
-                    } );
+                LeftMenu.init( $( "#left" ) );
+                ReqestData.init( LeftMenu, 'ajax/menuleft/' );
+                ReqestData.reqest();
+
+                if ( result.id != 0 )
+                    userName = `<div class="user"><div class="title_user">Вы зашли как:</div>${result.name} ${result.family}</div>`;
+                $( '#menu' ).find( 'ul' ).append( userName );
+
             } )
             .fail( ( result ) => alert( result.responseJSON.error ) );
     }
 
-    const print_mainmenu = ( menu ) => {
-        let st = '';
-        for ( let i = 0; i < menu.length; i ++ )
-            st += `	<li class="menu_childs1">
-					<a id="${menu[ i ].id_a}" href="${menu[ i ].id_a}">${menu[ i ].name}</a>
-				</li>`;
-        return st;
-    }
-
-    const print_menu = ( menu ) => {
-        let st = '';
-        let li_id = '';
-        for ( let i = 0; i < menu.length; i ++ ) {
-            let lm = menu[ i ][ 0 ];
-            if ( lm.li_id !== null ) li_id = " class=" + lm.li_id; else li_id = '';
-            st += `	<li${li_id}>
-					<a id="${lm.id_a}" href="${lm.id_a}">
-						<i class="${lm.icon}"></i>
-						${lm.name}
-					</a>
-				</li>`;
-            if ( menu[ i ].length > 1 ) {
-                let j = 1;
-                st += `<ul class="submenu hide-submenu">`;
-                while ( j < menu[ i ].length ) {
-                    console.log( menu[ i ][ j ] )
-                    var subLm = menu[ i ][ j ];
-                    st +=
-                        `<li>
-                            <a  id="${subLm.id_a}" href="${subLm.id_a}">
-                                <i class="${subLm.icon}"></i>
-                                ${subLm.name}
-                            </a>
-                        </li>`;
-                    j ++;
-                }
-                st += "</ul>";
-            }
-        }
-        return st;
-    }
+    $( '#loginform' ).submit( function ( e ) {
+        e.preventDefault();
+        registration( this );
+        login_form.dialog( "close" );
+    } );
+    $( '#menu' ).on( 'click', '#login', function ( e ) {
+        login_form.dialog( "open" );
+        e.preventDefault();
+    } );
+    $( '#menu' ).on( 'click', '#logout', function ( e ) {
+        unregistration();
+        e.preventDefault();
+    } );
 
 
 } );
