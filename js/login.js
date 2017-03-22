be19b3bb664de0a853bd83850ff3aa03ba60a8fd
ValikPk\valik <publicav@gmail.com>
2017-03-22 19:22:09 +0200

    Тестирование работы с typeSript

--- js/login.js
+++ js/login.js
@@ -1,171 +1,157 @@
-$( function () {
-    const menu = $( '#menu' );
-    const ReqestData = {
+$(function () {
+    var menu = $('#menu');
+    var ReqestData = {
         render: {},
         data: '',
         url: '',
         type: '',
-        init: function ( render, url, param = '', type = 'post' ) {
+        init: function (render, url, param, type) {
+            if (param === void 0) { param = ''; }
+            if (type === void 0) { type = 'post'; }
             this.data = param;
             this.render = render;
             this.url = url;
             this.type = type;
         },
         reqest: function () {
-            let me = this;
-            //noinspection JSUnresolvedVariable
-            $.ajax( { async: 'false', dataType: 'json', type: me.type, url: me.url, data: me.data } )
-                .done( ( result ) => {
-                    // console.log( result );
-                    this.render.doRun( result.data );
-                    this.render.render();
-                } )
-                .fail( ( result ) => alert( result.responseJSON.error ) );
-
-        },
+            var _this = this;
+            var me = this;
+            $.ajax({
+                dataType: 'json',
+                type: me.type,
+                url: me.url,
+                data: me.data
+            })
+                .done(function (result) {
+                _this.render.doRun(result.data);
+                _this.render.render();
+            })
+                .fail(function (result) { return alert(result.responseJSON.error); });
+        }
     };
-    const ReqestLeftMenu = Object.assign( {}, ReqestData );
-    const MainMenu = {
+    var ReqestLeftMenu = Object.create(ReqestData);
+    var MainMenu = {
         dest: {},
         user: {},
         html: '',
-        init: function ( dest, user = {} ) {
+        init: function (dest, user) {
+            if (user === void 0) { user = {}; }
             this.dest = dest;
             this.user = user;
         },
-        doRun: function ( data ) {
-            let st = '';
+        doRun: function (data) {
+            var st = '';
             st += '<ul>';
-            for ( let i = 0; i < data.length; i ++ ) { //noinspection JSUnresolvedVariable
-                st += `	<li class="menu_childs1">
-                                <a id="${data[ i ].id_a}" href="${data[ i ].id_a}">${data[ i ].name}</a>
-                            </li>
-            `;
+            for (var i = 0; i < data.length; i++) {
+                st += "\t<li class=\"menu_childs1\">\n                                <a id=\"" + data[i].id_a + "\" href=\"" + data[i].id_a + "\">" + data[i].name + "</a>\n                            </li>\n            ";
             }
             st += '</ul>';
             this.html = st;
         },
         render: function () {
-            let userName;
-            this.dest.html( this.html );
-            if ( (this.user.id != 0) && (Object.keys( this.user ).length != 0 )) { //noinspection JSUnresolvedVariable
-                userName = `<div class="user"><div class="title_user">Вы зашли как:</div>${this.user.name} ${this.user.family}</div>`;
+            var userName;
+            this.dest.html(this.html);
+            if ((this.user.id != 0) && (Object.keys(this.user).length != 0)) {
+                userName = "<div class=\"user\"><div class=\"title_user\">\u0412\u044B \u0437\u0430\u0448\u043B\u0438 \u043A\u0430\u043A:</div>" + this.user.name + " " + this.user.family + "</div>";
             }
-            this.dest.find( 'ul' ).append( userName );
-
+            this.dest.find('ul').append(userName);
         }
     };
-    const LeftMenu = {
+    var LeftMenu = {
         dest: {},
         html: '',
-        init: function ( dest ) {
+        init: function (dest) {
             this.dest = dest;
         },
-        doRun: function ( data ) {
-            let st = '';
-            let li_id = '';
-            let subLm;
-            let lm;
+        doRun: function (data) {
+            var st = '';
+            var li_id = '';
+            var subLm;
+            var lm;
             st += '<nav id="navbar"><ul id="left-menu">';
-            for ( let i = 0; i < data.length; i ++ ) {
-                lm = data[ i ][ 0 ];
-                //noinspection JSUnresolvedVariable
-                if ( lm.li_id !== null ) { //noinspection JSUnresolvedVariable
+            for (var i = 0; i < data.length; i++) {
+                lm = data[i][0];
+                if (lm.li_id !== null) {
                     li_id = " class=" + lm.li_id;
-                } else li_id = '';
-
-                //noinspection JSUnresolvedVariable
-                st += `	<li${li_id}>
-    			<a id="${lm.id_a}" href="${lm.id_a}">
-    				<i class="${lm.icon}"></i>
-    				${lm.name}
-    			</a>
-    		</li>`;
-                if ( data[ i ].length > 1 ) {
-                    let j = 1;
-                    st += `<ul class="submenu hide-submenu">`;
-                    while ( j < data[ i ].length ) {
-                        subLm = data[ i ][ j ];
-                        //noinspection JSUnresolvedVariable
+                }
+                else
+                    li_id = '';
+                st += "\t<li" + li_id + ">\n    \t\t\t<a id=\"" + lm.id_a + "\" href=\"" + lm.id_a + "\">\n    \t\t\t\t<i class=\"" + lm.icon + "\"></i>\n    \t\t\t\t" + lm.name + "\n    \t\t\t</a>\n    \t\t</li>";
+                if (data[i].length > 1) {
+                    var j = 1;
+                    st += "<ul class=\"submenu hide-submenu\">";
+                    while (j < data[i].length) {
+                        subLm = data[i][j];
                         st +=
-                            `<li>
-                            <a  id="${subLm.id_a}" href="${subLm.id_a}">
-                                <i class="${subLm.icon}"></i>
-                                ${subLm.name}
-                            </a>
-                        </li>`;
-                        j ++;
+                            "<li>\n                            <a  id=\"" + subLm.id_a + "\" href=\"" + subLm.id_a + "\">\n                                <i class=\"" + subLm.icon + "\"></i>\n                                " + subLm.name + "\n                            </a>\n                        </li>";
+                        j++;
                     }
                     st += "</ul>";
                 }
             }
-
             st += '</ul></nav>';
             this.html = st;
         },
         render: function () {
-            this.dest.html( this.html );
+            this.dest.html(this.html);
         }
     };
-    const login_form = $( '#loginmodal' ).dialog( {
+    var login_form = $('#loginmodal').dialog({
         title: "Регистрация пользователя",
         autoOpen: false,
         resizable: false,
         modal: true,
         height: 250,
         width: 400
-    } );
-    const Auth = {
+    });
+    var Auth = {
         mMenuTarget: '',
         lMenuTarget: '',
-        init: function ( mMenuTarget, lMenuTarget ) {
+        init: function (mMenuTarget, lMenuTarget) {
             this.mMenuTarget = mMenuTarget;
             this.lMenuTarget = lMenuTarget;
         },
         logout: function () {
-            $.ajax( { dataType: 'json', type: 'post', url: 'ajax/unregistration/' } )
-                .done( () => {
-                    MainMenu.init( this.mMenuTarget );
-                    ReqestData.init( MainMenu, 'json/menu.json' );
-                    ReqestData.reqest();
-
-                    this.lMenuTarget.html( '' );
-                    $( '#right' ).html( '' );
-                } )
-                .fail( () => alert( 'Error' ) );
+            var _this = this;
+            $.ajax({ dataType: 'json', type: 'post', url: 'ajax/unregistration/' })
+                .done(function () {
+                MainMenu.init(_this.mMenuTarget);
+                ReqestData.init(MainMenu, 'json/menu.json');
+                ReqestData.reqest();
+                _this.lMenuTarget.html('');
+                $('#right').html('');
+            })
+                .fail(function () { return alert('Error'); });
         },
-        login: function ( form ) {
-            let m_method = $( form ).attr( 'method' );
-            let m_action = $( form ).attr( 'action' );
-            let m_data = $( form ).serialize();
-
-            //noinspection JSUnresolvedVariable
-            $.ajax( { dataType: 'json', type: m_method, url: m_action, data: m_data } )
-                .done( ( result ) => {
-                    MainMenu.init( this.mMenuTarget, result );
-                    ReqestData.init( MainMenu, 'json/menu_registration.json' );
-                    ReqestData.reqest();
-
-                    LeftMenu.init( this.lMenuTarget );
-                    ReqestLeftMenu.init( LeftMenu, 'ajax/menuleft/' );
-                    ReqestLeftMenu.reqest();
-                } )
-                .fail( ( result ) => alert( result.responseJSON.error ) );
+        login: function (form) {
+            var _this = this;
+            var m_method = $(form).attr('method');
+            var m_action = $(form).attr('action');
+            var m_data = $(form).serialize();
+            $.ajax({ dataType: 'json', type: m_method, url: m_action, data: m_data })
+                .done(function (result) {
+                MainMenu.init(_this.mMenuTarget, result);
+                ReqestData.init(MainMenu, 'json/menu_registration.json');
+                ReqestData.reqest();
+                LeftMenu.init(_this.lMenuTarget);
+                ReqestLeftMenu.init(LeftMenu, 'ajax/menuleft/');
+                ReqestLeftMenu.reqest();
+            })
+                .fail(function (result) { return alert(result.responseJSON.error); });
         }
     };
-    $( '#loginform' ).submit( function ( e ) {
+    $('#loginform').submit(function (e) {
         e.preventDefault();
-        Auth.login( this );
-        login_form.dialog( "close" );
-    } );
-    menu.on( 'click', '#logout', function ( e ) {
+        Auth.login(this);
+        login_form.dialog("close");
+    });
+    menu.on('click', '#logout', function (e) {
         Auth.logout();
         e.preventDefault();
-    } );
-    menu.on( 'click', '#login', function ( e ) {
-        login_form.dialog( "open" );
+    });
+    menu.on('click', '#login', function (e) {
+        login_form.dialog("open");
         e.preventDefault();
-    } );
-
-    Auth.init( menu, $( "#left" ) );
-} );
+    });
+    Auth.init(menu, $("#left"));
+});
--- ts/login.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var $ = require("../node_modules/jquery/");
-var test = 10;
-$("#html").html(test);
